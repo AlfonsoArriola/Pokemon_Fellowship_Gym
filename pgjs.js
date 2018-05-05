855946b416a53ca24ec5beac46b1fd82ed784871
Erisay Perez <Erisaypr@gmail.com>
2018-05-04 23:14:01 -0400

    Solution

--- pgjs.js
+++ pgjs.js
@@ -1,80 +1,66 @@
 $(document).ready(function() {
 
     class Trainer {
-        constructor(trainerName, trainerNum, pokemonArr) {
-            this.trainerName = trainerName;
-            this.pokemons = pokemonArr;
-            this.trainerNum = trainerNum;
+        constructor() {
+            this.trainername = {
+
+            };
+            this.pokemons = {
+                bulbasaur: undefined,
+                charmander: undefined,
+                squirtle: undefined,
+                throh: undefined,
+                litten: undefined,
+                tyrunt: undefined,
+                larvitar: undefined,
+                vulpix: undefined,
+                mewtwo: undefined,
+            };
+
         }
-        get(pokemonName) {
+        get(pokemonName, secParent, trainerName) {
+            
             let self = this;
-        }
-    }
+             {
+                $.ajax({
+                    method: "GET",
+                    url: "https://pokeapi.co/api/v2/pokemon/" + pokemonName,
+                    success: function(data) {
+                        self.pokemons[pokemonName] = data;
+                        $(`section#${secParent} > div.card > div.card-body > h3#poke-name`).html(data.name);
+                        $(`section#${secParent} > div.card > div.card-body > table > tbody > tr > td#poke-hp`).html(data.stats[5].base_stat);
+                        $(`section#${secParent} > div.card > div.card-body > table > tbody > tr > td#poke-attack`).html(data.stats[4].base_stat);
+                        $(`section#${secParent} > div.card > div.card-body > table > tbody > tr > td#poke-defense`).html(data.stats[3].base_stat);
 
-    function getData(trainer, trainerNum, pokemon) {
-            let pokemonName = pokemon;
-            $.ajax({
-            method: "GET",
-            url: "https://pokeapi.co/api/v2/pokemon/" + pokemonName,
-            success: function(data) {
-                $("#poke-name-" + trainerNum).html(data.name); 
-                $("#poke-hp-" + trainerNum).html(data.stats[5].base_stat); // $('#' + trainerName + ' .hp').html('...)
-                $("#poke-attack-" + trainerNum).html(data.stats[4].base_stat);
-                $("#poke-defense-" + trainerNum).html(data.stats[3].base_stat);
-                let abilities = [];
-                for (var i = 0; i < data.abilities.length; i++) {
-                    abilities.push(data.abilities[i].ability.name + " ");
-                }
-                $("#poke-abilities-" + trainerNum).html(abilities);
-
+                        let abilities = [];
+                        for (var i = 0; i < data.abilities.length; i++) {
+                            abilities.push(data.abilities[i].ability.name + " ");
+                        }
+                        $(`section#${secParent} > div.card > div.card-body > table > tbody > tr > td#poke-abilities`).html(abilities);
                     }
                 });
+            }
+
+
+        }
+
+        }
+
+
+    let myTrainer = new Trainer;
+
+
+    $("#poke-trainer").html(myTrainer.trainername);
+
+    function onPokemonClicked(clickEvent) {
+        
+        let secParent = $(clickEvent.target).parents();
+        myTrainer.get(clickEvent.target.id, secParent[2].id);
     }
 
-    let silverdragonia = new Trainer('Silverdragonia', 1, [1, 4, 7]);
-    let spawnhellraiser = new Trainer('SpawnHellraiser', 2, [246, 37, 150]);
-    let juanito = new Trainer('Juanito', 3, [1, 4, 7]);
-    let cash_money = new Trainer('Cash_Money', 4, [696, 725, 538]);
+    $('.pokemon-button').click(onPokemonClicked);
 
-    $('#bulbasaur0').click(function(){
-        getData('silverdragonia',1,'bulbasaur')
-    });
-    $('#charmander0').click(function(){
-        getData('silverdragonia',1,'charmander')
-    });
-    $('#squirtle0').click(function(){
-        getData('silverdragonia',1,'squirtle')
-    });
-
-    $('#larvitar').click(function(){
-        getData('spawnhellraiser',2,'larvitar')
-    });
-    $('#vulpix').click(function(){
-        getData('spawnhellraiser',2,'vulpix')
-    });
-    $('#mewtwo').click(function(){
-        getData('spawnhellraiser',2,'mewtwo')
-    });
-
-    $('#bulbasaur1').click(function(){
-        getData('juanito',3,'bulbasaur')
-    });
-    $('#charmander1').click(function(){
-        getData('juanito',3,'charmander')
-    });
-    $('#squirtle1').click(function(){
-        getData('juanito',3,'squirtle')
-    });
-
-    $('#litten').click(function(){
-        getData('cash_money',4,'litten')
-    });
-    $('#tyrunt').click(function(){
-        getData('cash_money',4,'tyrunt')
-    });
-    $('#throh').click(function(){
-        getData('cash_money',4,'throh')
-    });
-
+    $('#myModal').modal();
 
 }); //JQuery
+  
